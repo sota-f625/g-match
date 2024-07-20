af252d2958cc20e88d95ac95ea8bac8e4feeac75
sota-f625 <s7716xf625@gmail.com>
2024-07-21 00:55:14 +0900

    チャットform編集

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import "./Chat.scss"
 import ChatHeader from './ChatHeader'
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import CardGiftcardIcon from '@mui/icons-material/CardGiftcard';
+import GifIcon from '@mui/icons-material/Gif';
+import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAlt';
 
 const Chat = () => {
   return (
@@ -10,7 +14,21 @@ const Chat = () => {
       {/* chatMessage */}
       <div className="chatMessage"></div>
       {/* chatInput */}
-      <div className="chatInput"></div>
+      <div className="chatInput">
+        <AddCircleOutlineIcon />
+        <form>
+          <input type="text" placeholder='#メッセージを送信' />
+          <button type ="submit" className="chatInputButton">
+            send
+          </button>
+        </form>
+
+        <div className="chatInputIcons">
+          <CardGiftcardIcon />
+          <GifIcon />
+          <SentimentSatisfiedAltIcon />
+        </div>
+      </div>
     </div>
   )
 }
