c2b2dbc806b3318074db8a9f55b9a33e77dbe0b4
sota-f625 <s7716xf625@gmail.com>
2024-07-21 10:26:55 +0900

    firebaseの設定

---
+++ src/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite"
+import { getAuth, GoogleAuthProvider } from "firebase/auth"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAzzPsm6yHHbybVV7_RpDn7acIgSqQIrYc",
+  authDomain: "g-match-3da4e.firebaseapp.com",
+  projectId: "g-match-3da4e",
+  storageBucket: "g-match-3da4e.appspot.com",
+  messagingSenderId: "1053917355070",
+  appId: "1:1053917355070:web:6eda5f79d2747f6cd2456f"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+
+export { auth, provider, db };
