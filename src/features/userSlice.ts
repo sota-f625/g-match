012d8dad15fb512cddbdc81764e0108ec99baa51
sota-f625 <s7716xf625@gmail.com>
2024-07-28 22:46:52 +0900

    userSlice編集

--- src/features/userSlice.ts
+++ src/features/userSlice.ts
@@ -6,11 +6,11 @@ export const userSlice = createSlice({
   reducers: {
     login: (state, action) => {
       state.user = action.payload;
-    };
+    },
     logout: (state) => {
       state.user = null;
-    };
-  }
+    },
+  },
 });
 
 export default userSlice.reducer;
