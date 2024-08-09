8212f22175d1d6966dd8d79a3ac7b06a1b8137cb
sota-f625 <s7716xf625@gmail.com>
2024-08-09 17:02:50 +0900

    チャット文字列取得

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Chat.scss"
 import ChatHeader from './ChatHeader'
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
@@ -9,6 +9,8 @@ import ChatMessage from './ChatMessage ';
 import { useAppSelector } from '../../app/hooks';
 
 const Chat = () => {
+  const [inputText, setInputText] = useState<string>("");
+
   const channelName = useAppSelector((state) => state.channel.channelName);
 
   return (
@@ -26,7 +28,7 @@ const Chat = () => {
       <div className="chatInput">
         <AddCircleOutlineIcon />
         <form>
-          <input type="text" placeholder='#メッセージを送信' />
+          <input type="text" placeholder='#メッセージを送信' onChange={(e: React.ChangeEvent<HTMLInputElement>) => setInputText(e.target.value)}/>
           <button type ="submit" className="chatInputButton">
             send
           </button>
